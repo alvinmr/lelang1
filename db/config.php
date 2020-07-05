bd113eec12f97bef494808a9414db8799f4abeb4
Alvin Maulana <33139640+alvinmr@users.noreply.github.com>
2020-07-05 23:43:23 +0800

    Update config.php

--- db/config.php
+++ db/config.php
@@ -1,9 +1,9 @@
 <?php
 
-$host = 'localhost';
-$user = 'root';
-$password = '';
-$db = 'lelang';
+$host = 'us-cdbr-east-02.cleardb.com';
+$user = 'b2855f1ea468df';
+$password = '99e409f4';
+$db = 'heroku_336cb94e119e4f5';
 $base_url = 'localhost';
 
 $koneksi = mysqli_connect($host, $user, $password, $db) or die('koneksi gagal');
